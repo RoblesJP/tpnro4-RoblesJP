d0dd0461d1ce1bfb079d533bc91791b01f800899
RoblesJP <juan.robles1417@gmail.com>
2020-10-06 22:33:33 -0300

    Agregar Main/

---
+++ Main/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Cadeteria;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empresa miCadeteria1 = Helper.GenerarEmpresaAleatoria(5, 5);
+            Helper.InformeDeEmpresa(miCadeteria1);
+        }
+    }
+}
