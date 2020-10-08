4b69d4500b532c12196a3ef434415a6b198e22d3
RoblesJP <juan.robles1417@gmail.com>
2020-10-08 01:48:52 -0300

    Agregar clase Persona

---
+++ Cadeteria/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cadeteria
+{
+    public class Persona
+    {
+        // atributos
+        private int id;
+        private string nombre;
+        private string direccion;
+        private long telefono;
+
+        // propiedades
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public long Telefono { get => telefono; set => telefono = value; }
+
+        // constructor
+        public Persona(string nombre, string direccion, long telefono)
+        {
+            Nombre = nombre;
+            Direccion = direccion;
+            Telefono = telefono;
+        }
+    }
+}
