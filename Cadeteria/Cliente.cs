887e17a7585eb3f9f4ae2b4f62b03019429f12f2
RoblesJP <juan.robles1417@gmail.com>
2020-10-08 02:01:45 -0300

    Agregar método RealizarPedido() y atributo listaDePedidosRealizados

--- Cadeteria/Cliente.cs
+++ Cadeteria/Cliente.cs
@@ -4,23 +4,45 @@ using System.Text;
 
 namespace Cadeteria
 {
-    public class Cliente
+    public class Cliente : Persona
     {
-        private int id;
-        private string nombre;
-        private string direccion;
-        private long telefono;
+        private static int nextId = 0;
+        // atributos
+        private List<Pedido> listaDePedidosRealizados;
 
-        public int Id { get => id; set => id = value; }
-        public string Nombre { get => nombre; set => nombre = value; }
-        public string Direccion { get => direccion; set => direccion = value; }
-        public long Telefono { get => telefono; set => telefono = value; }
+        // propiedades
+        public List<Pedido> ListaDePedidosRealizados { get => listaDePedidosRealizados; set => listaDePedidosRealizados = value; }
 
-        public Cliente(string nombre, string direccion, long telefono)
+        // constructor
+        public Cliente(string nombre, string direccion, long telefono) : base(nombre, direccion, telefono)
+        { 
+            Id = ++nextId;
+            ListaDePedidosRealizados = new List<Pedido>();
+        }
+
+        // métodos
+        public int CantidadDePedidosRealizados()
         {
-            Nombre = nombre;
-            Direccion = direccion;
-            Telefono = telefono;
+            return ListaDePedidosRealizados.Count;
+        }
+
+        public Pedido RealizarPedido(Tipo tipo, string descripcion)
+        {
+            Pedido suPedido = null;
+            switch (tipo)
+            {
+                case Tipo.Express:
+                    suPedido = new PedidoExpress(descripcion);
+                    break;
+                case Tipo.Delicado:
+                    suPedido = new PedidoDelicado(descripcion);
+                    break;
+                case Tipo.Ecologico:
+                    suPedido = new PedidoEcologico(descripcion);
+                    break;
+            }
+            ListaDePedidosRealizados.Add(suPedido);
+            return suPedido;
         }
     }
 }
