e0041876a25d73ec55c55e4008d55dde7f2fc2a8
RoblesJP <juan.robles1417@gmail.com>
2020-10-08 02:00:30 -0300

    Agregar atributo vehiculo y modificado el método Jornal()

--- Cadeteria/Cadete.cs
+++ Cadeteria/Cadete.cs
@@ -4,39 +4,55 @@ using System.Linq;
 
 namespace Cadeteria
 {
-    public class Cadete
+    public enum Vehiculo
     {
-        private int id;
-        private string nombre;
-        private string direccion;
-        private long telefono;
+        Bicicleta,
+        Moto,
+        Auto
+    }
+    public class Cadete : Persona
+    {
+        private static int nextId = 0;
+        // atributos
+        private Vehiculo vehiculo;
         List<Pedido> listaDePedidos;
 
-        public int Id { get => id; set => id = value; }
-        public string Nombre { get => nombre; set => nombre = value; }
-        public string Direccion { get => direccion; set => direccion = value; }
-        public long Telefono { get => telefono; set => telefono = value; }
+        // propiedades
         public List<Pedido> ListaDePedidos { get => listaDePedidos; set => listaDePedidos = value; }
+        public Vehiculo Vehiculo { get => vehiculo; set => vehiculo = value; }
 
-        public Cadete(int id, string nombre, string direccion, long telefono)
+        // constructor
+        public Cadete(string nombre, string direccion, long telefono, Vehiculo vehiculo) : base(nombre, direccion, telefono)
         {
-            Id = id;
-            Nombre = nombre;
-            Direccion = direccion;
-            Telefono = telefono;
+            Id = ++nextId;
+            Vehiculo = vehiculo;
             ListaDePedidos = new List<Pedido>();
         }
 
-        public void AgregarPedido(Pedido pedido)
+        // métodos
+        public void TomarPedido(Pedido pedido)
         {
             ListaDePedidos.Add(pedido);
+            pedido.Estado = Estado.Asignado;
         }
 
-        public int Jornal()
+        public double Jornal()
         {
-            int jornal = 0;
+            double jornal = 0;
             int cantidadDePedidosEnregados = (ListaDePedidos.Where(pedido => pedido.Estado == Estado.Entregado)).Count();
             jornal = cantidadDePedidosEnregados * 100;
+            switch(this.Vehiculo)
+            {
+                case Vehiculo.Bicicleta:
+                    jornal *= 1.05;
+                    break;
+                case Vehiculo.Moto:
+                    jornal *= 1.20;
+                    break;
+                case Vehiculo.Auto:
+                    jornal *= 1.25;
+                    break;
+            }
             return jornal;
         }
 
@@ -49,6 +65,11 @@ namespace Cadeteria
         public double PromedioDePedidosEntregados()
         {
             return (double) CantidadDePedidosEntregados() / (double) ListaDePedidos.Count;
+        }
+
+        public void EntregarPedido(Pedido suPedido)
+        {
+            suPedido.Estado = Estado.Entregado;
         }
     }
 }
