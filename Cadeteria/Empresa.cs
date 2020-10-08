8e0901cde5fa25302d236ebe71cd0bf2cd4a47a2
RoblesJP <juan.robles1417@gmail.com>
2020-10-08 02:02:21 -0300

    Agregar atributo listaDeClientes

--- Cadeteria/Empresa.cs
+++ Cadeteria/Empresa.cs
@@ -8,39 +8,45 @@ namespace Cadeteria
 {
     public class Empresa
     {
+        // atributos
         private string nombre;
         private List<Cadete> listaDeCadetes;
+        private List<Cliente> listaDeClientes;
 
+        // propiedades
         public string Nombre { get => nombre; set => nombre = value; }
-        public List<Cadete> ListadoDeCadetes { get => listaDeCadetes; set => listaDeCadetes = value; }
+        public List<Cadete> ListaDeCadetes { get => listaDeCadetes; set => listaDeCadetes = value; }
+        public List<Cliente> ListaDeClientes { get => listaDeClientes; set => listaDeClientes = value; }
 
+        // constructor
         public Empresa(string nombre)
         {
             Nombre = nombre;
-            ListadoDeCadetes = new List<Cadete>();
+            ListaDeCadetes = new List<Cadete>();
+            ListaDeClientes = new List<Cliente>();
         }
 
-        public void AgregarCadete(string nombre, string direccion, long telefono)
+        // métodos
+        public void AgregarCadete(string nombre, string direccion, long telefono, Vehiculo vehiculo)
         {
-            Cadete nuevoCadete = new Cadete(ListadoDeCadetes.Count + 1, nombre, direccion, telefono);
-            ListadoDeCadetes.Add(nuevoCadete);
+            Cadete nuevoCadete = new Cadete(nombre, direccion, telefono, vehiculo);
+            ListaDeCadetes.Add(nuevoCadete);
         }
 
-        public void MarcarPedidoComoEntregado(int idCadete, int nroPedido)
+        public void AgregarCliente(string nombre, string direccion, long telefono)
         {
-            Cadete miCadete = ListadoDeCadetes.Find(cadete => cadete.Id == idCadete);
-            Pedido suPedido = miCadete.ListaDePedidos.Find(pedido => pedido.Nro == nroPedido);
-            suPedido.Estado = Estado.Entregado;
+            Cliente nuevoCliente = new Cliente(nombre, direccion, telefono);
+            ListaDeClientes.Add(nuevoCliente);
         }
 
         public Cadete CadeteConMasPedidosEntregados()
         {
-            Cadete miCadete = ListadoDeCadetes[0];
-            for (int i =  1; i < ListadoDeCadetes.Count; i++)
+            Cadete miCadete = ListaDeCadetes[0];
+            for (int i =  1; i < ListaDeCadetes.Count; i++)
             {
-                if (ListadoDeCadetes[i].CantidadDePedidosEntregados() > miCadete.CantidadDePedidosEntregados())
+                if (ListaDeCadetes[i].CantidadDePedidosEntregados() > miCadete.CantidadDePedidosEntregados())
                 {
-                    miCadete = ListadoDeCadetes[i];
+                    miCadete = ListaDeCadetes[i];
                 }
             }
             return miCadete;
